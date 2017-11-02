572f890968294422354e3f3ec31f6fbc4e6498c2
alimehawesh <32651075+alimehawesh@users.noreply.github.com>
2017-11-03 00:36:55 +0100

    Add files via upload

---
+++ js2/js1.js
@@ -0,0 +1,68 @@
+var English = "hello world";
+console.log(English);
+var Arabic = "marhba";
+console.log(Arabic);
+var French = "bonjour";
+console.log(French);
+console.log("I'm awesome");
+let x;
+console.log("The value of x will be: Undefined");
+console.log(x);
+x = 5
+console.log("the value of my variable x will be : 5 ");
+console.log(x);
+let y = "Hi";
+console.log('the value of my string will be: hi');
+console.log(y);
+y = "Hoi";
+console.log('the value of my string will be: Hoi');
+console.log(y);
+let z = 7.25;
+console.log(z);
+let a = Math.round(z); 
+console.log(a);
+let h
+if (z > a) {
+    h = a ;
+    console.log(h);
+} 
+else {
+    h = a ;
+    console.log(h);
+    
+};
+let Animal = [];
+console.log('the value of the array is []') ;
+console.log(Animal);
+let theanimals =["lion","cat","dog"];
+console.log(theanimals);
+theanimals.push("baby pig");
+console.log(theanimals);
+let  myString = "this,is,a,test";
+console.log(myString);
+myString.length ;
+console.log(myString.length);
+let a = 40;
+let b = "good" ;
+let c = ["red","blue","black"] ;
+let d = true;
+console.log('The value of my variable a is: ' + a);
+console.log('The value of my variable b is: ' + b );
+console.log('The value of my variable c is: ' + c );
+console.log('The value of my variable d is: ' + d );
+typeof(a);
+typeof(b);
+typeof(c);
+typeof(d);
+if(typeof(a) == typeof(b)){
+    console.log("SAME TYPE");
+}
+else {
+    console.log("NOT SAME TYPE");
+};
+x = 15
+console.log(x = x % 5);
+x = 30
+console.log(x = x % 9);
+x = 75
+console.log(x = x % 25 );
